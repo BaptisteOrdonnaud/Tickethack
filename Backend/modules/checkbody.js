53abc60488f0671ad1bf02657beb2983c1e1cf72
Your name <Your email>
2024-01-30 20:36:39 +0100

    routes

--- Backend/modules/checkbody.js
+++ Backend/modules/checkbody.js
@@ -1,5 +1,6 @@
 const checkBody = (body, keys) => {
     for (let i = 0; i < keys.length; i++) {
+
         if (!body[keys[i]]) {
             return false
         }
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,27 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+
 const Trip = require('../models/trips');
 const { checkBody } = require('../modules/checkBody');
 
 
 /* GET home page. */
+
 router.get('/trips', (req, res) => {
-  const { departure, arrival, date } = req.body
+  let { departure, arrival, date } = req.body
+
+  const startDate = date + 'T00:00:00.001+00:00';
+  const endDate = date + 'T23:59:59.001+00:00';
+  const regexDeparture = new RegExp(departure, 'i');
+  const regexArrival = new RegExp(arrival, 'i');
 
 
   if (!checkBody(req.body, ["departure", "arrival", "date"])) {
     res.json({ error: 'Missing or empty fields' });
     return;
-  } Trip.findOne({ departure: departure, arrival: arrival, date: date }).then(dbData => {
-    if (dbData === null) {
-      res.json({ result: false, error: 'Trip not found' })
-    } else {
-
-      res.json({ result: dbData });
+  } Trip.find({
+    departure: regexDeparture,
+    arrival: regexArrival,
+    date: {
+      $gte: new Date(startDate),
+      $lte: new Date(endDate)
     }
-  });
+  })
+    .then(dbData => {
+
+      if (dbData === null) {
+        res.json({ result: false, error: 'Trip not found' })
+      } else {
+
+        res.json({ result: dbData });
+      }
+    });
 
 });
 
+
+// function ajouterAuPanier(){
+
+// }
 module.exports = router;
