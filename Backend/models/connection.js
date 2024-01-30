a6efe984eff1fbe41df40fa48c270bf0f978e02f
Baptiste Ordonnaud <baptisteordonnaud@gmail.com>
2024-01-30 10:05:46 +0100

    add connection require

--- Backend/models/connection.js
+++ Backend/models/connection.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-const connectionString = '';
+const connectionString = 'mongodb+srv://baptisteordonnaud:adxbCcT4m6JYwvYH@cluster0.16ilost.mongodb.net/tickethack';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
     .then(() => console.log('Database connected'))
