42a222f5264b045f05bfb4b90c697d490f2a186d
Your name <Your email>
2024-01-31 13:58:07 +0100

    button

--- Frontend/scripts/cart.js
+++ Frontend/scripts/cart.js
@@ -1,25 +1,3 @@
-// function Cart() {
-//     fetch('http://localhost:3000/users/cart')
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data.cart)
-//             if (data.users) {
-//                 for (let i = 0; i < data.cart.length; i++) {
-//                     document.querySelector('#state-trip-content').innerHTML += `
-//                         <div id="state-trip-cities">
-//                         <span id="city-A"></span>
-//                         <span>></span>
-//                         <span id="city-B"></span>
-//                     </div>
-//                     <span id="trip-hours"></span>
-//                     <span id="trip-price"></span>
-//                     <button id="trip-book">X</button>
-//                 </div>
-//                     `;
-//                 }
-//             } //updateDeleteCityEventListener();
-//         })
-// }
 
 function Cart() {
     fetch('http://localhost:3000/users/allTrips')
@@ -31,26 +9,59 @@ function Cart() {
                 document.querySelector('#container-cart-full').style.display = 'block'
 
                 for (let i = 0; i < data.allTrips.length; i++) {
+                    const id = data.allTrips[i].trip._id;
                     const departure = data.allTrips[i].trip.departure;
                     const arrival = data.allTrips[i].trip.arrival;
                     const date = data.allTrips[i].trip.date;
                     const price = data.allTrips[i].trip.price;
 
+                    // Convertir la date en objet Date
+                    const tripDate = new Date(date);
+
+                    // Obtenir l'heure au format HH:mm
+                    const tripTime = tripDate.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
+
+
+
                     document.querySelector('#state-trip-content').innerHTML += `
-                        <div class="state-trip-item">
-                            <div id="state-trip-cities">
-                                <span id="city-A">${departure}</span>
+                        
+                            <div class="state-trip-cities">
+                                <span class="city-A">${departure}</span>
                                 <span>></span>
-                                <span id="city-B">${arrival}</span>
+                                <span class="city-B">${arrival}</span>
                             </div>
-                            <span id="trip-hours">${date}</span>
-                            <span id="trip-price">${price}</span>
-                            <button class="trip-book">X</button>
-                        </div>
+                            <span class="trip-hours">${tripTime}</span>
+                            <span class="trip-price">${price}€</span>
+                            <button id="${id}" class="trip-book">X</button>
+                        
                     `;
                 }
+                deleteTrip()
             }
-            // Ajoutez ici l'appel à une fonction pour mettre à jour les gestionnaires d'événements pour les boutons "trip-book"
+
         })
 }
-Cart();
+
+Cart()
+
+
+function deleteTrip() {
+    for (let i = 0; i < document.querySelectorAll('#state-trip-content').length; i++) {
+        document.querySelectorAll('#trip-book')[i].addEventListener('click', () => {
+            const body = {
+                travelId: this.id
+            }
+            fetch(`http://localhost:3000/users/deleteTrip`, {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(body)
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.result) {
+                        this.parentNode.remove();
+                    }
+                });
+        });
+    }
+}
